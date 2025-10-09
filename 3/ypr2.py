a98098599ed6980faf24cb61ffe1a6a9f11e66c8
Oriksokol <oriksokol@gmail.com>
2025-10-09 17:59:58 +0300

    упр 2 доп

--- 3/ypr2.py
+++ 3/ypr2.py
@@ -1,6 +1,10 @@
 a =str(input())
 m=[]
+k=[]
+l=[]
+j=0
 b=int(a)
+g=b
 while b>1:
     for i in range(2,b+1):
         if b%i==0:
@@ -8,4 +12,14 @@ while b>1:
             b = b//i
             a=str(b)
             break
-print(m)
+for i in range(len(m)):
+    if i == range(len(m)):
+        output=[f"{k[i]}^{l[i]}" for i in range(len(k))]
+        print(f"{g} = {"*".join(output)}")
+        exit
+    if m[i] in k:
+       d=k.index[m[i]]
+       l[d]+=1
+    else:
+        k.append(m[i])
+        l.append(1)
